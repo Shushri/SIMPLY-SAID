c10e995791d7b6ceca265a0788fce7f6065db8ce
Shushri <shubhiarnm@gmail.com>
2025-06-09 23:45:46 +0530

    done with login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import bodyParser from "body-parser";
+import { name } from "ejs";
 const app=express();
 const port=3000;
 
@@ -7,8 +8,18 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+var user="";
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    
+    res.render("index.ejs"
+    );
+})
+
+app.post("/home",(req,res)=>{
+    user=req.body.fName;
+    res.render("about.ejs",
+        {name:user}
+    )
 })
 
 app.listen(port,()=>{
