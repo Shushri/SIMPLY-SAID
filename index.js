06e2777c4917411045942fa9f840bcf961b9c1ed
Shushri <shubhiarnm@gmail.com>
2025-06-10 14:20:07 +0530

    postt made

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.get("/",(req,res)=>{
     );
 })
 
+app.get("/blogs/:category", (req, res) => {
+  const category = req.params.category;
+
+  const validCategories = ["tech", "lifestyle", "education", "personal", "creative"];
+  
+  if (validCategories.includes(category)) {
+    res.render(`blogs/${category}.ejs`, { category: category });
+  } else {
+    res.status(404).send("Category not found");
+  }
+});
+
+
+
 app.post("/home",(req,res)=>{
     user=req.body.fName;
     res.render("about.ejs",
