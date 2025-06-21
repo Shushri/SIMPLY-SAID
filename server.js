b754e6a0d4a20cfb951c187451623aafb81ff424
Shushri <shubhiarnm@gmail.com>
2025-06-21 10:06:42 +0530

    creating API backend and frontend server separately

---
+++ server.js
@@ -0,0 +1,59 @@
+import express from "express"
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app=express();
+const port=3000;
+const API_URL = "http://localhost:4000";
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var user="";
+app.get("/",(req,res)=>{
+    try {
+      res.render("index.ejs");  
+    } catch (error) {
+      console.log("NOT FOUND");
+    }
+    
+})
+
+app.get("/home", async(req, res) => {
+  const category = req.params.category;
+  var number=Math.floor(Math.random()*100);
+  const validCategories = ["tech", "lifestyle", "education", "personal", "creative"];
+  try {
+    if (validCategories.includes(category)) {
+    res.render(`blogs/${category}.ejs`, { category: category  });
+  } else {
+    res.status(404).send("Category not found");
+  }  
+  } 
+  catch (error) {
+    if (validCategories.includes(category)) {
+    res.render(`blogs/${category}.ejs`, { category: category , facts:"waiting..." });
+  } else {
+    res.status(404).send("Category not found");
+  } 
+  }
+  
+  
+});
+
+
+
+app.post("/home",(req,res)=>{
+    user=req.body.fName;
+    res.render("about.ejs",
+        {name:user}
+    )
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Port ${port} is listening now...`);
+})
